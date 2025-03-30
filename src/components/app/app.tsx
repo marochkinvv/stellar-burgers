a9307303f192fe425431007dd833eed98e90522e
marochkinvv <marochkinvv@gmail.com>
2025-03-30 16:19:13 +0300

    Initial commit

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,76 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
 import { AppHeader } from '@components';
+import { ProtectedRoute } from '../protected-route/protected-route';
+import { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => (
+export const App: FC = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route
+        path='/login'
+        element={
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/register'
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/forgot-password'
+        element={
+          <ProtectedRoute>
+            <ForgotPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/reset-password'
+        element={
+          <ProtectedRoute>
+            <ResetPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile/orders'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
-
-export default App;
---
+++ src/components/protected-route/protected-route.ts
@@ -0,0 +1,7 @@
+type ProtectedRouteProps = {
+  children: React.ReactElement;
+};
+
+export const ProtectedRoute = ({
+  children
+}: ProtectedRouteProps): React.ReactElement => children;
